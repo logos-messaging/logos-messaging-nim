waku-org/nwaku

more temporary changes

library/waku_context.nim
@@ -3,38 +3,23 @@
 {.passc: "-fPIC".}
 
 import std/[options, atomics, os, net, locks]
-import chronicles, chronos, chronos/threadsync, taskpools/channels_spsc_single, results
+import
+  chronicles, chronos, chronos/threadsync, taskpools/channels_spsc_single, results, ffi
 import
   waku/factory/waku,
-  waku/node/peer_manager,
+  # waku/node/peer_manager,
   waku/waku_relay/[protocol, topic_health],
   waku/waku_core/[topics/pubsub_topic, message],
   ./waku_thread_requests/[waku_thread_request, requests/debug_node_request],
-  ./ffi_types,
   ./events/[
     json_message_event, json_topic_health_change_event, json_connection_change_event,
     json_waku_not_responding_event,
   ]
 
-type WakuContext* = object
-  wakuThread: Thread[(ptr WakuContext)]
-  watchdogThread: Thread[(ptr WakuContext)]
-    # monitors the Waku thread and notifies the Waku SDK consumer if it hangs
-  lock: Lock
-  reqChannel: ChannelSPSCSingle[ptr WakuThreadRequest]
-  reqSignal: ThreadSignalPtr
-    # to inform The Waku Thread (a.k.a TWT) that a new request is sent
-  reqReceivedSignal: ThreadSignalPtr
-    # to inform the main thread that the request is rx by TWT
-  userData*: pointer
-  eventCallback*: pointer
-  eventUserdata*: pointer
-  running: Atomic[bool] # To control when the threads are running
-
 const git_version* {.strdefine.} = "n/a"
 const versionString = "version / git commit hash: " & waku.git_version
 
-template callEventCallback(ctx: ptr WakuContext, eventName: string, body: untyped) =
+template callEventCallback(ctx: ptr FFIContext, eventName: string, body: untyped) =
   if isNil(ctx[].eventCallback):
     error eventName & " - eventCallback is nil"
     return
@@ -42,41 +27,41 @@ template callEventCallback(ctx: ptr WakuContext, eventName: string, body: untype
   foreignThreadGc:
     try:
       let event = body
-      cast[WakuCallBack](ctx[].eventCallback)(
+      cast[FFICallBack](ctx[].eventCallback)(
         RET_OK, unsafeAddr event[0], cast[csize_t](len(event)), ctx[].eventUserData
       )
     except Exception, CatchableError:
       let msg =
         "Exception " & eventName & " when calling 'eventCallBack': " &
         getCurrentExceptionMsg()
-      cast[WakuCallBack](ctx[].eventCallback)(
+      cast[FFICallBack](ctx[].eventCallback)(
         RET_ERR, unsafeAddr msg[0], cast[csize_t](len(msg)), ctx[].eventUserData
       )
 
-proc onConnectionChange*(ctx: ptr WakuContext): ConnectionChangeHandler =
-  return proc(peerId: PeerId, peerEvent: PeerEventKind) {.async.} =
-    callEventCallback(ctx, "onConnectionChange"):
-      $JsonConnectionChangeEvent.new($peerId, peerEvent)
+# proc onConnectionChange*(ctx: ptr FFIContext): ConnectionChangeHandler =
+#   return proc(peerId: PeerId, peerEvent: PeerEventKind) {.async.} =
+#     callEventCallback(ctx, "onConnectionChange"):
+#       $JsonConnectionChangeEvent.new($peerId, peerEvent)
 
-proc onReceivedMessage*(ctx: ptr WakuContext): WakuRelayHandler =
+proc onReceivedMessage*(ctx: ptr FFIContext): WakuRelayHandler =
   return proc(pubsubTopic: PubsubTopic, msg: WakuMessage) {.async.} =
     callEventCallback(ctx, "onReceivedMessage"):
       $JsonMessageEvent.new(pubsubTopic, msg)
 
-proc onTopicHealthChange*(ctx: ptr WakuContext): TopicHealthChangeHandler =
+proc onTopicHealthChange*(ctx: ptr FFIContext): TopicHealthChangeHandler =
   return proc(pubsubTopic: PubsubTopic, topicHealth: TopicHealth) {.async.} =
     callEventCallback(ctx, "onTopicHealthChange"):
       $JsonTopicHealthChangeEvent.new(pubsubTopic, topicHealth)
 
-proc onWakuNotResponding*(ctx: ptr WakuContext) =
+proc onWakuNotResponding*(ctx: ptr FFIContext) =
   callEventCallback(ctx, "onWakuNotResponsive"):
     $JsonWakuNotRespondingEvent.new()
 
 proc sendRequestToWakuThread*(
-    ctx: ptr WakuContext,
+    ctx: ptr FFIContext,
     reqType: RequestType,
     reqContent: pointer,
-    callback: WakuCallBack,
+    callback: FFICallBack,
     userData: pointer,
     timeout = InfiniteDuration,
 ): Result[void, string] =
@@ -114,10 +99,10 @@ proc sendRequestToWakuThread*(
   ## process proc. See the 'waku_thread_request.nim' module for more details.
   ok()
 
-proc watchdogThreadBody(ctx: ptr WakuContext) {.thread.} =
+proc watchdogThreadBody(ctx: ptr FFIContext) {.thread.} =
   ## Watchdog thread that monitors the Waku thread and notifies the library user if it hangs.
 
-  let watchdogRun = proc(ctx: ptr WakuContext) {.async.} =
+  let watchdogRun = proc(ctx: ptr FFIContext) {.async.} =
     const WatchdogStartDelay = 10.seconds
     const WatchdogTimeinterval = 1.seconds
     const WakuNotRespondingTimeout = 3.seconds
@@ -128,7 +113,7 @@ proc watchdogThreadBody(ctx: ptr WakuContext) {.thread.} =
       await sleepAsync(WatchdogTimeinterval)
 
       if ctx.running.load == false:
-        debug "Watchdog thread exiting because WakuContext is not running"
+        debug "Watchdog thread exiting because FFIContext is not running"
         break
 
       let wakuCallback = proc(
@@ -152,10 +137,10 @@ proc watchdogThreadBody(ctx: ptr WakuContext) {.thread.} =
 
   waitFor watchdogRun(ctx)
 
-proc wakuThreadBody(ctx: ptr WakuContext) {.thread.} =
+proc wakuThreadBody(ctx: ptr FFIContext) {.thread.} =
   ## Waku thread that attends library user requests (stop, connect_to, etc.)
 
-  let wakuRun = proc(ctx: ptr WakuContext) {.async.} =
+  let wakuRun = proc(ctx: ptr FFIContext) {.async.} =
     var waku: Waku
     while true:
       await ctx.reqSignal.wait()
@@ -179,10 +164,10 @@ proc wakuThreadBody(ctx: ptr WakuContext) {.thread.} =
 
   waitFor wakuRun(ctx)
 
-proc createWakuContext*(): Result[ptr WakuContext, string] =
+proc createWakuContext*[T](): Result[ptr FFIContext, string] =
   ## This proc is called from the main thread and it creates
   ## the Waku working thread.
-  var ctx = createShared(WakuContext, 1)
+  var ctx = createShared(FFIContext, 1)
   ctx.reqSignal = ThreadSignalPtr.new().valueOr:
     return err("couldn't create reqSignal ThreadSignalPtr")
   ctx.reqReceivedSignal = ThreadSignalPtr.new().valueOr:
@@ -205,7 +190,7 @@ proc createWakuContext*(): Result[ptr WakuContext, string] =
 
   return ok(ctx)
 
-proc destroyWakuContext*(ctx: ptr WakuContext): Result[void, string] =
+proc destroyWakuContext*(ctx: ptr FFIContext): Result[void, string] =
   ctx.running.store(false)
 
   let signaledOnTime = ctx.reqSignal.fireSync().valueOr:

logos-messaging/logos-messaging-nim

library/waku_context.nim
@@ -3,39 +3,24 @@
 {.passc: "-fPIC".}
 
 import std/[options, atomics, os, net, locks]
-import chronicles, chronos, chronos/threadsync, taskpools/channels_spsc_single, results
+import
+  chronicles, chronos, chronos/threadsync, taskpools/channels_spsc_single, results, ffi
 import
   waku/common/logging,
   waku/factory/waku,
-  waku/node/peer_manager,
+  # waku/node/peer_manager,
   waku/waku_relay/[protocol, topic_health],
   waku/waku_core/[topics/pubsub_topic, message],
   ./waku_thread_requests/[waku_thread_request, requests/debug_node_request],
-  ./ffi_types,
   ./events/[
     json_message_event, json_topic_health_change_event, json_connection_change_event,
     json_waku_not_responding_event,
   ]
 
-type WakuContext* = object
-  wakuThread: Thread[(ptr WakuContext)]
-  watchdogThread: Thread[(ptr WakuContext)]
-    # monitors the Waku thread and notifies the Waku SDK consumer if it hangs
-  lock: Lock
-  reqChannel: ChannelSPSCSingle[ptr WakuThreadRequest]
-  reqSignal: ThreadSignalPtr
-    # to inform The Waku Thread (a.k.a TWT) that a new request is sent
-  reqReceivedSignal: ThreadSignalPtr
-    # to inform the main thread that the request is rx by TWT
-  userData*: pointer
-  eventCallback*: pointer
-  eventUserdata*: pointer
-  running: Atomic[bool] # To control when the threads are running
-
 const git_version* {.strdefine.} = "n/a"
 const versionString = "version / git commit hash: " & waku.git_version
 
-template callEventCallback(ctx: ptr WakuContext, eventName: string, body: untyped) =
+template callEventCallback(ctx: ptr FFIContext, eventName: string, body: untyped) =
   if isNil(ctx[].eventCallback):
     error eventName & " - eventCallback is nil"
     return
@@ -43,41 +28,41 @@ template callEventCallback(ctx: ptr WakuContext, eventName: string, body: untype
   foreignThreadGc:
     try:
       let event = body
-      cast[WakuCallBack](ctx[].eventCallback)(
+      cast[FFICallBack](ctx[].eventCallback)(
         RET_OK, unsafeAddr event[0], cast[csize_t](len(event)), ctx[].eventUserData
       )
     except Exception, CatchableError:
       let msg =
         "Exception " & eventName & " when calling 'eventCallBack': " &
         getCurrentExceptionMsg()
-      cast[WakuCallBack](ctx[].eventCallback)(
+      cast[FFICallBack](ctx[].eventCallback)(
         RET_ERR, unsafeAddr msg[0], cast[csize_t](len(msg)), ctx[].eventUserData
       )
 
-proc onConnectionChange*(ctx: ptr WakuContext): ConnectionChangeHandler =
-  return proc(peerId: PeerId, peerEvent: PeerEventKind) {.async.} =
-    callEventCallback(ctx, "onConnectionChange"):
-      $JsonConnectionChangeEvent.new($peerId, peerEvent)
+# proc onConnectionChange*(ctx: ptr FFIContext): ConnectionChangeHandler =
+#   return proc(peerId: PeerId, peerEvent: PeerEventKind) {.async.} =
+#     callEventCallback(ctx, "onConnectionChange"):
+#       $JsonConnectionChangeEvent.new($peerId, peerEvent)
 
-proc onReceivedMessage*(ctx: ptr WakuContext): WakuRelayHandler =
+proc onReceivedMessage*(ctx: ptr FFIContext): WakuRelayHandler =
   return proc(pubsubTopic: PubsubTopic, msg: WakuMessage) {.async.} =
     callEventCallback(ctx, "onReceivedMessage"):
       $JsonMessageEvent.new(pubsubTopic, msg)
 
-proc onTopicHealthChange*(ctx: ptr WakuContext): TopicHealthChangeHandler =
+proc onTopicHealthChange*(ctx: ptr FFIContext): TopicHealthChangeHandler =
   return proc(pubsubTopic: PubsubTopic, topicHealth: TopicHealth) {.async.} =
     callEventCallback(ctx, "onTopicHealthChange"):
       $JsonTopicHealthChangeEvent.new(pubsubTopic, topicHealth)
 
-proc onWakuNotResponding*(ctx: ptr WakuContext) =
+proc onWakuNotResponding*(ctx: ptr FFIContext) =
   callEventCallback(ctx, "onWakuNotResponsive"):
     $JsonWakuNotRespondingEvent.new()
 
 proc sendRequestToWakuThread*(
-    ctx: ptr WakuContext,
+    ctx: ptr FFIContext,
     reqType: RequestType,
     reqContent: pointer,
-    callback: WakuCallBack,
+    callback: FFICallBack,
     userData: pointer,
     timeout = InfiniteDuration,
 ): Result[void, string] =
@@ -113,10 +98,10 @@ proc sendRequestToWakuThread*(
   ## process proc. See the 'waku_thread_request.nim' module for more details.
   ok()
 
-proc watchdogThreadBody(ctx: ptr WakuContext) {.thread.} =
+proc watchdogThreadBody(ctx: ptr FFIContext) {.thread.} =
   ## Watchdog thread that monitors the Waku thread and notifies the library user if it hangs.
 
-  let watchdogRun = proc(ctx: ptr WakuContext) {.async.} =
+  let watchdogRun = proc(ctx: ptr FFIContext) {.async.} =
     const WatchdogStartDelay = 10.seconds
     const WatchdogTimeinterval = 1.seconds
     const WakuNotRespondingTimeout = 3.seconds
@@ -127,7 +112,7 @@ proc watchdogThreadBody(ctx: ptr WakuContext) {.thread.} =
       await sleepAsync(WatchdogTimeinterval)
 
       if ctx.running.load == false:
-        info "Watchdog thread exiting because WakuContext is not running"
+        info "Watchdog thread exiting because FFIContext is not running"
         break
 
       let wakuCallback = proc(
@@ -151,12 +136,12 @@ proc watchdogThreadBody(ctx: ptr WakuContext) {.thread.} =
 
   waitFor watchdogRun(ctx)
 
-proc wakuThreadBody(ctx: ptr WakuContext) {.thread.} =
+proc wakuThreadBody(ctx: ptr FFIContext) {.thread.} =
   ## Waku thread that attends library user requests (stop, connect_to, etc.)
 
   logging.setupLog(logging.LogLevel.DEBUG, logging.LogFormat.TEXT)
 
-  let wakuRun = proc(ctx: ptr WakuContext) {.async.} =
+  let wakuRun = proc(ctx: ptr FFIContext) {.async.} =
     var waku: Waku
     while true:
       await ctx.reqSignal.wait()
@@ -179,10 +164,10 @@ proc wakuThreadBody(ctx: ptr WakuContext) {.thread.} =
 
   waitFor wakuRun(ctx)
 
-proc createWakuContext*(): Result[ptr WakuContext, string] =
+proc createWakuContext*[T](): Result[ptr FFIContext, string] =
   ## This proc is called from the main thread and it creates
   ## the Waku working thread.
-  var ctx = createShared(WakuContext, 1)
+  var ctx = createShared(FFIContext, 1)
   ctx.reqSignal = ThreadSignalPtr.new().valueOr:
     return err("couldn't create reqSignal ThreadSignalPtr")
   ctx.reqReceivedSignal = ThreadSignalPtr.new().valueOr:
@@ -205,7 +190,7 @@ proc createWakuContext*(): Result[ptr WakuContext, string] =
 
   return ok(ctx)
 
-proc destroyWakuContext*(ctx: ptr WakuContext): Result[void, string] =
+proc destroyWakuContext*(ctx: ptr FFIContext): Result[void, string] =
   ctx.running.store(false)
 
   let signaledOnTime = ctx.reqSignal.fireSync().valueOr: